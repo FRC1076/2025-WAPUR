3b8bb8dd80f576ab83d5349073b38f563726198d
kdh462009 <83740781+kdh462009@users.noreply.github.com>
2025-11-05 23:52:44 +0000

    Did Yoinker but need check Need to do some edit on Constant.java

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOHardware.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOHardware.java
@@ -11,6 +11,7 @@ import static frc.robot.Constants.ElevatorConstants.Control.kProfileConstraints;
 
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.config.SparkMaxConfig;
@@ -32,10 +33,42 @@ public class ElevatorIOHardware implements ElevatorIO
     
     private final RelativeEncoder m_encoder;
 
-    public ElevatorIOHardware() 
+    public ElevatorIOHardware() /*need edit*/
     {
-        m_leadMotor = new SparkMax(ElevatorConstants.kMotorPort0, MotorType.kBrushless);
-        m_followMotor = new SparkMax(ElevatorConstants.kMotorPort1, MotorType.kBrushless)
+        m_leadMotor = new SparkMax(ElevatorConstants.kMotorPort0, SparkMax.MotorType.kBrushless);
+        m_followMotor = new SparkMax(ElevatorConstants.kMotorPort1, SparkMax.MotorType.kBrushless);
+
+        m_leadMotorConfig = new SparkMaxConfig();
+        m_followMotorConfig = new SparkMaxConfig();
+
+        m_leadMotor.setCANTimeout(250);
+        m_followMotor.setCANTimeout(250);
+
+        m_leadMotorConfig
+            .inverted(ElevatorConstants.leadMotorInverted)
+            .smartCurrentLimit((int) ElevatorConstants.Electrical.kCurrentLimit)
+            .voltageCompensation(ElevatorConstants.Electrical.kVoltageCompensation);
+
+        m_leadMotorConfig.encoder
+            .positionConversionFactor(ElevatorConstants.kPositionConversionFactor)
+            .velocityConversionFactor(ElevatorConstants.kVelocityConversionFactor)
+            .quadratureMeasurementPeriod(10)
+            .quadratureAverageDepth(2);
+
+        m_followMotorConfig
+            .smartCurrentLimit((int) ElevatorConstants.Electrical.kCurrentLimit)
+            .voltageCompensation(ElevatorConstants.Electrical.kVoltageCompensation)
+            .follow(m_leadMotor, ElevatorConstants.followMotorInverted != ElevatorConstants.leadMotorInverted);
+        
+        m_leadMotor.configure(m_leadMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+        m_followMotor.configure(m_followMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+        m_encoder = m_leadMotor.getEncoder();
+
+        m_encoder.setPosition(0);
+
+        m_leadMotor.setCANTimeout(0);
+        m_followMotor.setCANTimeout(0);
     }
 
     @Override
