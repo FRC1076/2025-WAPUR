298f4ea0e9944d175937e4461df42e3909bd8e1a
pikitty314 <pikitty314@users.noreply.github.com>
2025-11-12 16:28:29 -0500

    Add in library

---
+++ src/main/java/lib/utils/MathHelpers.java
@@ -0,0 +1,33 @@
+// Copyright (c) FRC 1076 PiHi Samurai
+// You may use, distribute, and modify this software under the terms of
+// the license found in the root directory of this project
+
+package lib.utils;
+
+public final class MathHelpers {
+    /**
+     * Clamps a value between a minimum and maximum value.
+     * @param value The value to clamp.
+     * @param min The minimum value.
+     * @param max The maximum value.
+     * @return The clamped value.
+     */
+    public static double clamp(double value, double min, double max) {
+        if (min > max) {
+            throw new IllegalArgumentException("min must be less than max");
+        }
+
+        return Math.max(min, Math.min(max, value));
+    }
+
+    /**
+     * Average of two doubles.
+     * 
+     * @param firstValue First double.
+     * @param secondValue Second double.
+     * @return The average.
+     */
+    public static double getAverage(double firstValue, double secondValue) {
+        return (firstValue + secondValue) / 2;
+    }
+}
